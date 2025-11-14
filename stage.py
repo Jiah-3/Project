9d35c67a9f050bb176d945dba6569f292e37ea6c
Kim Jiah <2024184004@tukorea.ac.kr>
2025-11-14 18:39:04 +0900

    스테이지 순서 변경 / 추가

--- stage.py
+++ stage.py
@@ -31,6 +31,22 @@ def set_stage1_1():
     positions = [(0, 30), (100, 30), (200, 30),
                  (300, 30), (400, 30), (500, 30),
                  (600, 30), (700, 30), (800, 30),
+                 ]
+    grounds = [Ground() for _ in positions]
+    for ground, (x, y) in zip(grounds, positions):
+        ground.x = x
+        ground.y = y
+        game_world.add_object(ground, 1)
+        game_world.add_collision_pair('char:ground', None, ground)
+    bg = Bg()
+    bg.set_bg(1)
+    game_world.add_object(bg, 0)
+
+def set_stage1_2():
+    global positions
+    positions = [(0, 30), (100, 30), (200, 30),
+                 (300, 30), (400, 30), (500, 30),
+                 (600, 30), (700, 30), (800, 30),
 
                  (150, 130), (250, 130),
                  (550, 130), (650, 130),
@@ -45,7 +61,7 @@ def set_stage1_1():
     bg.set_bg(1)
     game_world.add_object(bg, 0)
 
-def set_stage1_2():
+def set_stage1_3():
     global positions
     positions = [(0, 30), (100, 30), (200, 30),
                  (300, 30), (400, 30), (500, 30),
