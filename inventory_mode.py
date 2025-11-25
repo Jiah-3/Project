6ece84ad1cffbe51184b28a3a56538454d0680ed
Kim Jiah <2024184004@tukorea.ac.kr>
2025-11-25 15:55:26 +0900

    스텟 찍기 설정

--- inventory_mode.py
+++ inventory_mode.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+import character_state
 import game_framework
 import game_world
 from inventory import Inventory
@@ -16,6 +17,53 @@ def handle_events():
             game_framework.pop_mode()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_e:
             game_framework.pop_mode()
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            event.y = 600 + event.y * -1
+            if event.x >= 20 and event.x <= 170:
+                global inventory
+                if inventory.char.stat_points > 0:
+                    if event.y <= 512 and event.y >= 482:
+                        inventory.char.stat_hp += 1
+                        inventory.char.max_hp += 1
+                        inventory.char.hp += 1
+                        inventory.char.stat_points -= 1
+                        character_state.char.stat_hp += 1
+                        character_state.char.max_hp += 1
+                        character_state.char.hp += 1
+                        character_state.char.stat_points -= 1
+
+                    if event.y <= 462 and event.y >= 432:
+                        inventory.char.stat_attack += 1
+                        inventory.char.damage += 0.05
+                        inventory.char.stat_points -= 1
+                        character_state.char.stat_attack += 1
+                        character_state.char.damage += 0.05
+                        character_state.char.stat_points -= 1
+                    if event.y <= 412 and event.y >= 382:
+                        inventory.char.stat_defense += 1
+                        inventory.char.defense += 0.5
+                        inventory.char.stat_points -= 1
+                        character_state.char.stat_defense += 1
+                        character_state.char.defense += 0.5
+                        character_state.char.stat_points -= 1
+                    if event.y <= 362 and event.y >= 332:
+                        inventory.char.stat_agility += 1
+                        inventory.char.speed += 1
+                        inventory.char.dodge += 0.1
+                        inventory.char.stat_points -= 1
+                        character_state.char.stat_agility += 1
+                        character_state.char.speed += 1
+                        character_state.char.dodge += 0.1
+                        character_state.char.stat_points -= 1
+                    if event.y <= 312 and event.y >= 282:
+                        inventory.char.stat_luck += 1
+                        inventory.char.crit_chance += 1
+                        inventory.char.stat_points -= 1
+                        character_state.char.stat_luck += 1
+                        character_state.char.crit_chance += 1
+                        character_state.char.stat_points -= 1
+
+            print(f"Left click at: ({event.x}, {event.y})")
 
 def finish():
     global inventory
